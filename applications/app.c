e088b9e6aef50b263bf44fb5d72b4fa6820d7dc9
livello <livello@bk.ru>
2022-04-19 13:24:14 +0300

    PAS pins

--- applications/app.c
+++ applications/app.c
@@ -121,12 +121,16 @@ void app_set_configuration(app_configuration *conf) {
 		break;
 
 	case APP_PAS:
-		app_pas_start(true);
+        app_pas_start(true);
+        hw_stop_i2c();
+        app_uartcomm_start(UART_PORT_COMM_HEADER);
 		break;
 
 	case APP_ADC_PAS:
 		app_adc_start(false);
-		app_pas_start(false);
+		app_pas_start(true);
+		hw_stop_i2c();
+		app_uartcomm_start(UART_PORT_COMM_HEADER);
 		break;
 
 	case APP_NRF:
