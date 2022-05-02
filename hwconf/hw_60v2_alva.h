e38dd68e29326b6e9a117cdd76c45b080d651b3e
livello <livello@bk.ru>
2022-05-02 13:40:48 +0300

    change adc_ext to adc_15 motor_temp_s2

--- hwconf/hw_60v2_alva.h
+++ hwconf/hw_60v2_alva.h
@@ -106,11 +106,11 @@
 #define ADC_IND_CURR2			4
 #define ADC_IND_CURR3			5
 #define ADC_IND_VIN_SENS		11
-#define ADC_IND_EXT				6
+#define ADC_IND_EXT				10
 #define ADC_IND_EXT2			7
 #define ADC_IND_TEMP_MOS		8
 #define ADC_IND_TEMP_MOTOR		9
-#define ADC_IND_TEMP_MOTOR_S2	10
+#define ADC_IND_TEMP_MOTOR_S2	6
 #define ADC_IND_VREFINT			12
 
 // ADC macros and settings
